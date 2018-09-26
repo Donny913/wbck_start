acb2f395ba9da0968f65bd54c3b2e61efd544b8d
oleg.miniuk@gmail.com <oleg.miniuk@gmail.com>
2018-09-26 16:54:16 +0300

    file and url loader

--- config/commonWebpackModules.js
+++ config/commonWebpackModules.js
@@ -8,7 +8,29 @@ const commonWebpackModules = {
       test: /\.(js|jsx)$/,
       exclude: /node_modules/,
       loader: 'babel-loader'
-    }
+    },
+    {
+      test: /\.(png|jpg|gif|ico)$/,
+      use: [
+        {
+          loader: 'file-loader',
+          options: { name: 'images/[name].[ext]' }
+        }
+      ]
+    },
+    {
+      test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+      use: [
+        {
+          loader: 'url-loader',
+          options: {
+            limit: 8192,
+            mimetype: 'application/font-woff',
+            name: 'fonts/[name].[ext]'
+          }
+        }
+      ]
+    },
   ]
 };
 
